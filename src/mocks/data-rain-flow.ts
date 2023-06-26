52897cc595466588cf66e34d390df294ce3ae103
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-06-26 08:12:45 +0300

    fix: data-rain

---
+++ src/mocks/data-rain-flow.ts
@@ -0,0 +1,21 @@
+const data = {
+  place: 1,
+  condition: 1,
+  area: {
+    roof: 1,
+    stone: 1,
+    lawns: 1,
+    tracks: 1,
+    ground: 1,
+    pavements: 1,
+    cobblestone: 1,
+  },
+  intensity: 1,
+  lengthPipe: 1,
+  lengthTray: 1,
+  velocityPipe: 1,
+  velocityTray: 1,
+  timeInit: 1,
+};
+
+export default data;
