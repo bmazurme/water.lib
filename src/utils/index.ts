6b5a363833d2d61038d99ab85cbeef905ec23556
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-06-27 19:11:48 +0300

    fix: utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,25 +1,29 @@
-import convertJouleToCalories from './convert-joule-to-calories';
+import index from './convert-joule-to-calories';
 import getA from './get-a';
 import getAreaSumm from './get-area-summ';
 import getFlow from './get-flow';
 import getIndex from './get-index';
 import getN from './get-n';
 import getQMidOrMax from './get-q-mid-or-max';
+import getSummRoofArea from './get-summ-roof-area';
 import getPa from './get-pa';
+import getQ5 from './get-q5';
 import getTimePipeOrTray from './get-time-pipe-or-tray';
 import getTimeSumm from './get-time-summ';
 import getZMid from './get-z-mid';
 import getZ from './get-z';
 
 export {
-  convertJouleToCalories,
+  index,
   getA,
   getAreaSumm,
   getFlow,
   getIndex,
   getN,
   getQMidOrMax,
+  getSummRoofArea,
   getPa,
+  getQ5,
   getTimePipeOrTray,
   getTimeSumm,
   getZMid,
